6256a2a779b58b4357339a6a6bb1a8c2a40899a5
edinson0810 <lache081015@gmail.com>
2024-10-01 21:34:03 -0500

    01/10/2024

---
+++ ejercicio5.js
@@ -0,0 +1,8 @@
+let edad = prompt("Ingrese su edad");
+let sueldo = prompt("Ingrese su sueldo");
+
+if ( edad > 16 && sueldo >= 1000){
+    alert("Debe Tributar")
+} else {
+    alert("No Debe Tributar")
+}
